d2ad88de21c77ec4d52adafc8bbdf63a442f5840
Joseph Matimba <jdgmatimba@gmail.com>
2022-07-01 11:16:22 -0700

    Add 2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+*main - prints alphabet in lowercase followed by new line
+*Return: returns 0 if successful and non-zero if fail
+*/
+
+int main(void)
+{
+int alpha;
+for (alpha = 'a'; alpha <= 'z'; alpha++)
+{
+	alpha = tolower(alpha);
+	putchar(alpha);
+}
+return (0);
+}
