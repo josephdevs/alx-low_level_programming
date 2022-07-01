246d459c25397d7feb2df410a92ec930b60b1fe4
Joseph Matimba <jdgmatimba@gmail.com>
2022-06-30 19:50:23 -0700

    add

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Prints" and that piece of art is useful - Dora Korpar, 2015-10-19"
+ * Return: standard error
+ */
+int main(void)
+{
+printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+return (0);
+}
