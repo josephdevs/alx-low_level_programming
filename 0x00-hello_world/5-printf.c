ed2e682e8eccd6e83e1b305573a243a3e0a7be59
Joseph Matimba <jdgmatimba@gmail.com>
2022-06-30 19:03:42 -0700

    Add description for function main

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
-
+/**
+ * main - prints "with proper grammar, but the outcome is a piece of art,"
+ * Return: 0 if there are no errors, other if errors found
+ */
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
