7586ce46a7f627ecf59b3d18b2af920276c72412
Joseph Matimba <jdgmatimba@gmail.com>
2022-06-30 18:41:56 -0700

    Had to please betty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
-
+/**
+ *main - prints text
+ *Return: 0 if no errors
+ */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
+puts("\"Programming is like building a multilingual puzzle\n");
 return (0);
 }
