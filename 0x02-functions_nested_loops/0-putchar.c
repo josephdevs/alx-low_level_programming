7863b2f6f644fb39207b3330b4e8c469b78070da
Joseph Matimba <jdgmatimba@gmail.com>
2022-07-05 13:20:03 -0700

    Add 0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ *main - prints exactly '_putchar' onto the screen.
+ *Return: returns 0 on successful run
+ */
+
+int main(void)
+{
+printf("_putchar\n");
+
+return (0);
+}
