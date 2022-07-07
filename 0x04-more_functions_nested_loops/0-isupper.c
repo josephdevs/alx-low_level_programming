cce37c617932099e9e6864ca9c204cf7fbe10a51
Joseph Matimba <jdgmatimba@gmail.com>
2022-07-07 07:31:59 -0700

    Add empty files for the tasks

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _isupper - Checks if c represents is uppercase.
+ * @c: Int representing a character
+ *Return: returns 1 if c is uppercase and 0 otherwise.
+ */
+
+int _isupper(int c)
+{
+
+	if (c >= 65 && c <= 90)
+
+	{
+		return (1);
+	}
+
+return (0);
+
+}
