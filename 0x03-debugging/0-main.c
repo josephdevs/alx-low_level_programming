7a4b09d6637dbdd66ae843a642891f6d81c76691
Joseph Matimba <jdgmatimba@gmail.com>
2022-07-07 18:29:32 -0700

    Add advanced tasks 0, 1 and 2 + README.md

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - tests if positive_or_negative() gives correct output when fed 0
+ * Return: returns 0 if program successful and !0 otherwise
+ */
+
+int main(void)
+
+{
+
+int i;
+
+i = 0;
+positive_or_negative(i);
+
+return (0);
+
+}
