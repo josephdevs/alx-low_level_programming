a4130ff55808ca12e1d23cd1a9d9debb509dabfd
Joseph Matimba <jdgmatimba@gmail.com>
2022-07-12 15:09:45 -0700

    Add README.md, main.h & task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+*reset_to_98 - takes pointer and updates value pointed to 98
+*@n - pointer to an integer 
+*Return: void
+*/
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
+
