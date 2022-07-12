b647a856bb2f81ede1c00e0692d036503160ea97
Joseph Matimba <jdgmatimba@gmail.com>
2022-07-12 15:21:02 -0700

    Modify 1-swap.c

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -2,14 +2,14 @@
 
 /**
 *swap_int - swaps values of two integers
-*@a and @b: integers '*a' and '*b'
+*@a: integer '*a'
+*@b: integer '*b'
 *Return: void
 */
 
 void swap_int(int *a, int *b)
 {
-	*a = tempNum;
-	//where 'tempNum' is a placeholder variable to hold value of '*a'
-	*a = *b;
-	*b = tempNum;
+	*a += *b;
+	*b = *a - *b;
+	*a = *a - *b;
 }
