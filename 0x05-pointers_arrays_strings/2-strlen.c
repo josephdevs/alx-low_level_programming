7f465800842ffacbcb102e846bb8b1064b04a321
Joseph Matimba <jdgmatimba@gmail.com>
2022-07-12 15:32:38 -0700

    modify 2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -3,7 +3,7 @@
 /**
  * _strlen - finds and returns the length of a string
  * @s: string '*s'
- * Return: 0
+ * Return: value of i
  */
 
 int _strlen(char *s)
@@ -12,5 +12,5 @@ int _strlen(char *s)
 
 	while (s[i])
 		i++;
-	return (0);
+	return (i);
 }
