79085efaecaf2ad164a338ae5f8c256a93797f70
Joseph Matimba <jdgmatimba@gmail.com>
2022-07-14 19:58:19 +0200

    Add task 6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,36 @@
+#include "main.h"
+/**
+ *cap_string - capitalises all words of string
+ *@s: points to string
+ *Return: pointer to s
+ */
+
+char *cap_string(char *s)
+{
+	int i;
+
+	i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[0] >= 97 && s[0] <= 122)
+		{
+			s[0] = s[0] - 32;
+		}
+
+		if (s[i] == ' ' || s[i] == '\t' || s[i] == '\n'
+		    || s[i] == ',' || s[i] == ';' || s[i] == '.'
+		    || s[i] == '.' || s[i] == '!' || s[i] == '?'
+		    || s[i] == '"' || s[i] == '(' || s[i] == ')'
+		    || s[i] == '{' || s[i] == '}')
+		{
+			if (s[i + 1] >= 97 && s[i + 1] <= 122)
+			{
+				s[i + 1] = s[i + 1] - 32;
+			}
+		}
+		i++;
+	}
+
+return (s);
+}
