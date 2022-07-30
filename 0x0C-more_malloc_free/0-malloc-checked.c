0ec004f67da52316041ce60ca758f1087b88032a
Joseph Matimba <jdgmatimba@gmail.com>
2022-07-30 04:01:55 +0200

    Add README.md, main.h & task 0

---
+++ 0x0C-more_malloc_free/0-malloc-checked.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ *void *malloc_checked - allocates memory using malloc
+ *
+ *@b: num of bytes
+ *
+ * Return: pointer to allocated memory
+ *
+ *
+ *
+ *
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	char *p
+
+	p = malloc(b);
+
+	if (p == NULL)
+	{
+		exit(98);
+	}
+
+	return (p);
+}
