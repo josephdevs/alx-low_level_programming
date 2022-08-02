bb60f5fbe647bbe354b2d094a5dd530544e9e77e
Joseph Matimba <jdgmatimba@gmail.com>
2022-08-02 03:21:25 +0200

    Make changes to include guards

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,6 +1,3 @@
-#ifndef 3_FUNCTION_LIKE_MACRO_H
-#define 3_FUNCTION_LIKE_MACRO_H
-
+#ifndef ABS(x)
 #define ABS(x) (((x) < 0) ? -(x) : (x))
-
 #endif
--- 0x0D-preprocessor/4-sum.h
+++ 0x0D-preprocessor/4-sum.h
@@ -1,6 +1,5 @@
-#ifndef 4_SUM_H
-#define 4_SUM_H
-
+#ifndef SUM
+#define SUM
 #define SUM(x, y) (x + y)
 
 #endif
